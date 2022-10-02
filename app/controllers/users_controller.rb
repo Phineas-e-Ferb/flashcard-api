576cbed4f784f99766b971639c7dea75ddcbba4a
sudjoao <isudjoao@gmail.com>
2022-10-02 16:58:03 -0300

    feat: create index, show and create methods for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: %i[ show ]
+
+    def index
+        @user = User.all
+        render json: @user
+    end
+
+    def show
+        render json: {username: @user.username}
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: { message: "User was created" }
+        end
+    end
+
+    private
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:username, :password)
+    end
 end
