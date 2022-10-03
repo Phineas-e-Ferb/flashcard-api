f77008254e5c4edde8fa515c9d2408f4466c5345
sudjoao <isudjoao@gmail.com>
2022-10-03 20:21:11 -0300

    fix: solve bcrypt loop problem
    Co-authored-By: Gui <200037994@aluno.unb.br>
    Co-authored-By: Rafael <rafael.cleydson@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,53 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: %i[ show ]
+  before_action :set_user, only: [:show, :update, :destroy]
 
-    def index
-        @user = User.all
-        render json: @user
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    params = user_params
+    @user = User.new({username: params[:username]})
+    @user.password = params[:password]
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
+  end
 
-    def show
-        render json: {username: @user.username}
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
+  end
 
-    def create
-        params = user_params
-        @user = User.new({ username: params[:username] })
-        @user.password = params[:password]
-        if @user.save
-            render json: { message: "User was created" }
-        end
-    end
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
 
-    private
+  private
+    # Use callbacks to share common setup or constraints between actions.
     def set_user
-        @user = User.find(params[:id])
+      @user = User.find(params[:id])
     end
 
+    # Only allow a trusted parameter "white list" through.
     def user_params
-        params.permit(:username, :password)
+      params.require(:user).permit(:username, :password_hash)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_02_194446) do
+ActiveRecord::Schema.define(version: 2022_10_03_231842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_hash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
